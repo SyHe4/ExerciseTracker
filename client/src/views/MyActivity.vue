d8499ca72ebe85e1ab76758109eba352b4a72f2e
SyHe4 <143753565+SyHe4@users.noreply.github.com>
2023-10-21 23:37:21 -0400

    activity page

--- client/src/views/MyActivity.vue
+++ client/src/views/MyActivity.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+    import { ref } from 'vue'
+    const open = ref(false)
 </script>
 
 <template>
@@ -9,16 +11,70 @@
     </div>
     <div class="columns">
         <div class="column is-half is-offset-one-quarter">
-            <button class="button is-dark is-full-width" style="background-color: rgb(204, 51, 51); color: white;">
+            <button class="button is-dark" style="background-color: rgb(204, 51, 51); color: white;" @click="open = true">
                 Add Workout
             </button>
+            <div v-if="open" class="modal">
+                <div class="field">
+                <label class="label">Title</label>
+                <div class="control">
+                    <input class="input" type="text">
+                </div>
+                </div>
+
+                <div class="field">
+                <label class="label">Date</label>
+                <div class="control">
+                    <input class="input" type="date" placeholder="mm/dd/yyyy">
+                </div>
+                </div>
+                <div class="field">
+                <label class="label">Duration</label>
+                <div class="control">
+                    <input class="input" type="text">
+                </div>
+                </div>
+                <div class="field">
+                <label class="label">Location</label>
+                <div class="control">
+                    <input class="input" type="text">
+                </div>
+                </div>
+                <div class="field">
+                <label class="label">Picture</label>
+                <div class="control">
+                    <input class="input" type="text">
+                </div>
+                </div>
+                <div class="field">
+                <label class="label">Type</label>
+                <div class="select">
+                    <select>
+                        <option>Run</option>
+                        <option>Bike</option>
+                        <option>Strength</option>
+                        <option>Cardio</option>
+                        <option>Walk</option>
+                    </select>
+                </div>
+                </div>
+                <button @click="open = false">Save changes</button>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
 .title {
-    color: #4a4a4a;
+    color: #383838;
     padding-left: 10vh;
+}
+.modal {
+  position: fixed;
+  z-index: 999;
+  top: 20%;
+  left: 50%;
+  width: 300px;
+  margin-left: -150px;
 }
 </style>
