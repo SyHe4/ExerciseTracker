f90b7fe63d0aaaead99aac7dec9f09a31454d491
SyHe4 <143753565+SyHe4@users.noreply.github.com>
2023-12-05 10:51:32 -0500

    i can spell require now

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,4 @@
-const express = requre('express');
+const express = require('express');
 const { getAll, get, search, create, update, remove } = require('../models/posts');
 const router = express.Router();
 
--- server/models/users.js
+++ server/models/users.js
@@ -1,5 +1,5 @@
 const data = require("../data/users.json");
-const jwt = requre('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 
